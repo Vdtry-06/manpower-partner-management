ed71001b8f4742be10c8ac6932ad90dcea868ca9
Vdtry-06 <tunhoipro0306@gmail.com>
2025-10-08 15:50:54 +0700

    add PartnerManager, Accountant

---
+++ src/main/java/com/vdtry06/partner_management/source/server/entities/PartnerManager.java
@@ -0,0 +1,11 @@
+package com.vdtry06.partner_management.source.server.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.io.Serializable;
+
+@Table(name = "tblPartnerManager")
+@Entity
+public class PartnerManager extends CompanyStaff implements Serializable {
+}
