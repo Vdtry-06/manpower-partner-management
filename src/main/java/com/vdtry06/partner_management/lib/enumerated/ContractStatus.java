0222d4825532f06529ba45f4121d12ff20703874
Vdtry-06 <tunhoipro0306@gmail.com>
2025-10-08 15:51:51 +0700

    add ContractStatus enum

---
+++ src/main/java/com/vdtry06/partner_management/lib/enumerated/ContractStatus.java
@@ -0,0 +1,8 @@
+package com.vdtry06.partner_management.lib.enumerated;
+
+public enum ContractStatus {
+    PENDING, // đang chờ
+    ACTIVE, // đang hoạt động
+    INACTIVE, // không hoạt động
+    CANCELED // hủy hợp đồng giữa chừng
+}
