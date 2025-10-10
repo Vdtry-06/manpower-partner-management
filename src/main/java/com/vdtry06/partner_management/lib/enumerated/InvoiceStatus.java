f64efcff9671ebc0d9502b640c3dc1aa37b35f03
Vdtry-06 <tunhoipro0306@gmail.com>
2025-10-10 23:16:38 +0700

    update properties in InvoiceStatus enum

---
+++ src/main/java/com/vdtry06/partner_management/lib/enumerated/InvoiceStatus.java
@@ -0,0 +1,7 @@
+package com.vdtry06.partner_management.lib.enumerated;
+
+public enum InvoiceStatus {
+    UNPAID, // Hoá đơn chưa được thanh toán
+    PARTIALLY_PAID, // Hoá đơn đã được thanh toán một phần
+    PAID // Hoá đơn đã được thanh toán đầy đủ
+}
--- src/main/java/com/vdtry06/partner_management/lib/enumerated/InvoiceType.java
+++
@@ -1,6 +0,0 @@
-package com.vdtry06.partner_management.lib.enumerated;
-
-public enum InvoiceType {
-    FULL_PAYMENT, // thanh toán toàn bộ
-    INSTALLMENT;  // thanh toán theo đợt / ca
-}
