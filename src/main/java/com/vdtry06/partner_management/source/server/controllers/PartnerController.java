d56649c3fedf77e0c52b967fd021e4cd4ac1c649
Vdtry-06 <tunhoipro0306@gmail.com>
2025-10-13 23:32:24 +0700

    update partner to get All Partners With Condition By Name

--- src/main/java/com/vdtry06/partner_management/source/server/controllers/PartnerController.java
+++ src/main/java/com/vdtry06/partner_management/source/server/controllers/PartnerController.java
@@ -1,9 +1,13 @@
 package com.vdtry06.partner_management.source.server.controllers;
 
 import com.vdtry06.partner_management.lib.api.ApiResponse;
+import com.vdtry06.partner_management.lib.api.PaginationResponse;
+import com.vdtry06.partner_management.lib.utils.PagingUtil;
+import com.vdtry06.partner_management.lib.utils.StringUtil;
 import com.vdtry06.partner_management.source.server.payload.partner.PartnerRequest;
 import com.vdtry06.partner_management.source.server.payload.partner.PartnerResponse;
 import com.vdtry06.partner_management.source.server.service.PartnerService;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,7 +20,16 @@ public class PartnerController {
     }
 
     @PostMapping("/add")
+    @PreAuthorize("hasAuthority('PARTNER_MANAGER')")
     public ApiResponse<PartnerResponse> createPartner(@RequestBody PartnerRequest partnerRequest) {
         return new ApiResponse<PartnerResponse>(true, partnerService.createPartner(partnerRequest));
+    }
+
+    @GetMapping
+    public PaginationResponse<PartnerResponse> getAllPartnersWithConditions(
+            @RequestParam(required = false, defaultValue = PagingUtil.DEFAULT_PAGE) int page,
+            @RequestParam(required = false, defaultValue = PagingUtil.DEFAULT_SIZE) int perPage,
+            @RequestParam(required = false, defaultValue = StringUtil.EMPTY) String search ) {
+        return partnerService.getAllPartnersWithConditions(page, perPage, search);
     }
 }
