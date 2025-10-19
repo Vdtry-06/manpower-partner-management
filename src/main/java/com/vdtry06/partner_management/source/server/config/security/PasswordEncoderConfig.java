4c07a33c5edce808559c8d62004619d01924491a
Vdtry-06 <tunhoipro0306@gmail.com>
2025-10-19 19:05:13 +0700

    move from config to

--- src/main/java/com/vdtry06/partner_management/source/server/config/security/PasswordEncoderConfig.java
+++ src/main/java/com/vdtry06/partner_management/source/server/config/security/PasswordEncoderConfig.java
@@ -1,4 +1,4 @@
-package com.vdtry06.partner_management.source.server.config;
+package com.vdtry06.partner_management.source.server.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
